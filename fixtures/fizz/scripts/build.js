0ed50fe9f5d09602a224a5f40fcc9930e211bf10
simo-vargasp-sonarsource <121958530+simo-vargasp-sonarsource@users.noreply.github.com>
2023-03-01 10:20:23 +0100

    Update build.js

--- fixtures/fizz/scripts/build.js
+++ fixtures/fizz/scripts/build.js
@@ -16,13 +16,13 @@ const isProduction = process.env.NODE_ENV === 'production';
 rimraf.sync(path.resolve(__dirname, '../build'));
 webpack(
   {
-    mode: isProduction ? 'production' : 'development',
-    devtool: isProduction ? 'source-map' : 'cheap-module-source-map',
-    entry: [path.resolve(__dirname, '../src/index.js')],
+   // mode: isProduction ? 'production' : 'development',
+   // devtool: isProduction ? 'source-map' : 'cheap-module-source-map',
+   // entry: [path.resolve(__dirname, '../src/index.js')],
     output: {
       path: path.resolve(__dirname, '../build'),
       filename: 'main.js',
-    },
+    //},
     module: {
       rules: [
         {
