89403f3cc5f25f9c7b1028d517e45d6a39020553
simo-vargasp-sonarsource <121958530+simo-vargasp-sonarsource@users.noreply.github.com>
2023-08-08 13:18:36 +0200

    Update jsx-dev-runtime.js

--- fixtures/legacy-jsx-runtimes/react-16/jsx-dev-runtime.js
+++ fixtures/legacy-jsx-runtimes/react-16/jsx-dev-runtime.js
@@ -5,3 +5,17 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   module.exports = require('./cjs/react-jsx-dev-runtime.development.js');
 }
+if(unexpectedCondition) {
+  alert("Unexpected Condition");
+}
+
+if(process.env.NODE_ENV === 'production') {
+  alert("Unexpected Condition");
+}
+else {
+  alert("Unexpected Condition");
+}
+
+if (condition)  // Noncompliant
+doTheThing();
+doTheOtherThing(); // Was the intent to call this function unconditionally?
