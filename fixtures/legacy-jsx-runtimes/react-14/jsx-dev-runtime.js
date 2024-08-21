68e1924bf10c07c1239bc4dc46e54dc13a26859b
simo-vargasp-sonarsource <121958530+simo-vargasp-sonarsource@users.noreply.github.com>
2024-08-21 11:17:00 +0200

    Update jsx-dev-runtime.js

--- fixtures/legacy-jsx-runtimes/react-14/jsx-dev-runtime.js
+++ fixtures/legacy-jsx-runtimes/react-14/jsx-dev-runtime.js
@@ -1,3 +1,6 @@
+#define 
+#include 
+  
 'use strict';
 
 if (process.env.NODE_ENV === 'production') {
@@ -5,3 +8,19 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   module.exports = require('./cjs/react-jsx-dev-runtime.development.js');
 }
+
+const numbers = [10, 2, 30, 1, 5];
+numbers.sort(); // Noncompliant: lexicographic sort
+console.log(numbers); // Output: [1, 10, 2, 30, 5]
+
+
+CREATE OR REPLACE PACKAGE PACK IS
+  TYPE mytype IS RECORD (
+    var1 mytable.mycolumn%TYPE -- Noncompliant
+  );
+
+  FUNCTION MY_FUNC(param1 IN mytable.mycolumn%TYPE) RETURN VARCHAR2; -- Noncompliant
+
+  FUNCTION MY_FUNC2(param1 IN mytable%ROWTYPE) RETURN VARCHAR2; -- Noncompliant
+
+END;
