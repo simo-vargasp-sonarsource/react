69158faaaf05e3844def9eb31010229ed3e35bef
simo-vargasp-sonarsource <121958530+simo-vargasp-sonarsource@users.noreply.github.com>
2024-08-21 11:19:22 +0200

    Update jsx-dev-runtime.js

--- fixtures/legacy-jsx-runtimes/react-16/jsx-dev-runtime.js
+++ fixtures/legacy-jsx-runtimes/react-16/jsx-dev-runtime.js
@@ -5,3 +5,8 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   module.exports = require('./cjs/react-jsx-dev-runtime.development.js');
 }
+/* some comment, end comment marker accidentally omitted
+// Make sure this function is called in a thread safe context
+Perform_Critical_Safety_Function(X);
+...
+/* this comment is non-compliant */
